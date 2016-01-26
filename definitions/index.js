b44914b457053d680a3b1b8f99c64a2f35aa2ea7
Pedro Narciso García Revington <p.revington@gmail.com>
2016-01-26 11:19:01 +0100

    included the signature and client parms

--- definitions/index.js
+++ definitions/index.js
@@ -3,4 +3,8 @@
 var definitions = {
     'v1': require('./version1')
 };
+definitions.querystring = {
+    sig: 'hmac signature',
+    cli: 'your client ID'
+};
 exports = module.exports = definitions;
